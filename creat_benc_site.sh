b0a9a651dcf8c6c4e695a8de3921b2b30f8abe35
JRII972 <59899019+JRII972@users.noreply.github.com>
2023-05-15 15:25:53 -0400

    Update creat_benc_site.sh

--- creat_benc_site.sh
+++ creat_benc_site.sh
@@ -9,15 +9,15 @@ cd daaf-bench
 
 sudo bench setup production
 
-# bench set-config -g db_host mariadb
-# bench set-config -g redis_cache redis://redis-cache:6379
-# bench set-config -g redis_queue redis://redis-queue:6379
-# bench set-config -g redis_socketio redis://redis-socketio:6379
+bench set-config -g db_host mariadb
+bench set-config -g redis_cache redis://redis-cache:6379
+bench set-config -g redis_queue redis://redis-queue:6379
+bench set-config -g redis_socketio redis://redis-socketio:6379
 
-bench new-site daaf.localhost --db-name daaf_bench --mariadb-root-password 123 --admin-password admin 
+bench new-site sisep.daaf --db-name daaf_bench --mariadb-root-password 123 --admin-password admin 
 
-bench --site daaf.localhost set-config developer_mode 1
-bench --site daaf.localhost clear-cache
+bench --site sisep.daaf set-config developer_mode 1
+bench --site sisep.daaf clear-cache
 
 bench get-app https://github.com/frappe/wiki
 bench get-app https://github.com/frappe/insights --branch main
